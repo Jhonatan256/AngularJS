eaf56d1768b3d5aae9ed3e53fb5e8c05a4d8ed5d
Jhonatan Andres <jhot.256@gmail.com>
2019-05-17 11:37:32 -0500

    the first component angular 6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ThisComponent } from './first.component/first.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ThisComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/first.component/first.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component ({
+  selector : 'app-prueba',
+  template : '<h2>work work</h2>',
+  styles : ['h2{color:red;}']
+})
+
+export class ThisComponent { }
